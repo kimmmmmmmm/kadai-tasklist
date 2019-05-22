27bc1ff20096485f945c7cce90b52bfe5a3c90fe
mayu.kimura <kimura@techacademy.jp>
2019-05-22 04:28:24 +0000

    validation

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,7 +4,6 @@
 
     <h1>Edit the task id: {{ $task->id }}</h1>
     
-    
     <div class="row">
         <div class="col-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
